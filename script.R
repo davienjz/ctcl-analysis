03223698518dfcaee65c8818c6af780ae6b40f31
Duncan J Murray <duncan.murray@cantab.net>
2017-05-18 16:37:37 +0100

    Another test commit

--- script.R
+++ script.R
@@ -6,3 +6,10 @@ panel3 <- read.csv("panel3.csv")
 panel4 <- read.csv("panel4.csv")
 panel5 <- read.csv("panel5.csv")
 
+#Merge data
+
+df <- rbind(panel1, panel2, panel3, panel4, panel5)
+
+write.csv(df, file = "workingfile.csv")
+
+
