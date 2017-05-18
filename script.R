87e01ad6398aa3657c01826617ef3a956dbeb442
JackLMc <jacklukemcmurray@gmail.com>
2017-05-18 16:40:58 +0100

    Jack's commit

--- script.R
+++ script.R
@@ -10,6 +10,6 @@ panel5 <- read.csv("panel5.csv")
 
 df <- rbind(panel1, panel2, panel3, panel4, panel5)
 
-write.csv(df, file = "workingfile.csv")
+write.csv(df, file = "workingfile.csv") #the file
 
 
