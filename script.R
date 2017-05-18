3dad0ca0fa924829d6d59d66850b4c49e82c3e87
Duncan J Murray <duncan.murray@cantab.net>
2017-05-18 15:37:44 +0100

    added 5 lines of code to test

---
+++ script.R
@@ -0,0 +1,8 @@
+#Import data
+
+panel1 <- read.csv("panel1.csv")
+panel2 <- read.csv("panel2.csv")
+panel3 <- read.csv("panel3.csv")
+panel4 <- read.csv("panel4.csv")
+panel5 <- read.csv("panel5.csv")
+
