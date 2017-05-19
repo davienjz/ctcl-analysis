8fcce945fcd6c2518c8ffe5a3d2c0cab07ec1285
Duncan J Murray <duncan.murray@cantab.net>
2017-05-19 10:28:44 +0100

    fixed filename issues due to typo

--- script.R
+++ script.R
@@ -8,7 +8,7 @@ library(EnvStats)
 library(xtable)
 library(corrgram)
 
-#import data
+### import data
 
 panel1 <- read.csv("panel1.csv")
 panel2 <- read.csv("panel2.csv")
@@ -16,13 +16,13 @@ panel3 <- read.csv("panel3.csv")
 panel4 <- read.csv("panel4.csv")
 panel5 <- read.csv("panel5.csv")
 
-#merge data
+### merge data
 
 df <- rbind(panel1, panel2, panel3, panel4, panel5)
 
-write.csv(df, file = "workingfile.csv") #the file
+write.csv(df, file = "workingfile.csv")
 
-#filename generation
+### filename generation
 
 count <<- 0
 gen <- function(x){
@@ -31,15 +31,43 @@ gen <- function(x){
 	paste0(countf,x,".png")
 }
 
-#extract sample number info
+### extract sample number info
 
-pos <- regexpr("CTCL[[:digit:]]{3}[A-Z]{2}[ ]?[[:digit:]]{2}[A-Z]?",df$Data.Set)
-samplestring <- substr(df$Data.Set,pos,pos+attributes(pos)[[1]]-1)
-pos2 <- regexpr("CTCL[[:digit:]]{3}[A-Z]{2}",samplestring)
+# find CTCL or CTL (typo) plus rest of string 
+pos <- regexpr(
+							 "CT[C]?L[[:digit:]]{3}[A-Z]{2}[ ]?[[:digit:]]{2}[A-Z]{0,2}",
+							 df$Data.Set,
+							 perl = TRUE)
+
+samplestring_unfixed <- substr(df$Data.Set,pos,pos+attributes(pos)[[1]]-1)
+
+# remove CTCL or CTL
+pos_fix <- regexpr("(?<=L)[[:digit:]]{3}[A-Z]{2}[ ]?[[:digit:]]{2}[A-Z]{0,2}",
+									 samplestring_unfixed,
+									 perl = TRUE)
+
+samplestring <- substr(samplestring_unfixed,pos_fix,pos_fix+attributes(pos_fix)[[1]]-1)
+
+# add CTCL back
+samplestring <- paste0("CTCL",samplestring)
+
+pos2 <- regexpr(
+								"CT[C]?L[[:digit:]]{3}[A-Z]{0,2}",
+								samplestring,
+								perl = TRUE)
+
+# find patient number
 patientnumber <- substr(samplestring,pos2,pos2+attributes(pos2)[[1]]-1)
+
+# find sample number
 samplenumber <- gsub(" ","",samplestring, fixed=TRUE)
 
+# check results
 levels(as.factor(patientnumber))
 levels(as.factor(samplenumber))
+
+df <- cbind(patientnumber,samplenumber,df)
+
+df[df$patientnumber=="CTCL013DB",]
 
 
