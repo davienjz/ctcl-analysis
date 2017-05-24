b93afd9cc557112506c7f82bf2e678222264c665
JackLMc <jacklukemcmurray@gmail.com>
2017-05-24 13:47:27 +0100

    diversity file ready for merge into main dataframe

--- diversity.R
+++ diversity.R
@@ -1,20 +1,33 @@
 #load in the data
-df_div <- read.csv("data/simpsons_data.csv")
+
+df_div <- read.csv("data/simpsons_data.csv", fileEncoding = "UTF-8-BOM")
+
+### import functions
+
+source("functions.R")
 
 ### extract sample number info
 
 # find CTCL plus rest of string 
+
 pos <- regexpr(
   "CTCL[[:digit:]]{3}[A-Z]{2}[ ]?[[:digit:]]{2}[A-S | U-Z]{0,1}",
   df_div$Data.Set,
-  perl = TRUE)
+  perl = TRUE) #don't include 'T', some strings are continuous with TCRVB e.g. CTCL008PB01TCRVB --> CTCL008PB01T
 
 samplestring <- substr(df_div$Data.Set,pos,pos+attributes(pos)[[1]]-1)
 
 #find the sample number
-diversitynum <- gsub(" ","",samplestring, fixed=TRUE)
-diversitynumber <- levels(as.factor(diversitynum))
-diversitynumber
-### import functions
-source("functions.R")
-writeCsv(diversitynumber)
+diversitynumber <- gsub(" ","",samplestring, fixed=TRUE)
+diversitynum <- levels(as.factor(diversitynumber))
+
+#load linking file
+link <- read.csv("data/diversity_link.csv")
+
+#bind columns
+diversity <- cbind(df_div, diversitynumber)
+head(diversity)
+
+#merge files
+diversity_df <- merge(diversity,link,by="diversitynumber")
+diversity_df
